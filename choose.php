68686ccf8ce3978924221a57590d75785bce0e73
aliyu7442 <149768449+aliyu7442@users.noreply.github.com>
2024-01-08 12:36:39 +0100

    Add files via upload

---
+++ choose.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!--
+		*******************************************************************************
+
+		BY SABIU YAU OF JIGPOLY
+
+		*******************************************************************************
+	-->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Chat System</title>
+<body bgcolor="green">
+    <font size="5pt" face="Calibri" color="black">
+</head><center>
+Hello you are Higly Welcome, this is a simple chatting system created in order to make information transmission very easily please don't use this as an opportunity to abuse,verbal maltreatment, assault, displaying attempts to harbour irrelevant materials/links, violation or  crime if found with one of the followings you will definately charge!!!
+<br />
+<p><h1>Choose Below to Continue</h1><a><strong>
+<a href="@@cht">Group Chat</p></a>
+
+
+</br></br><a href="logout.php">Log Out</a><br>
+<body>
+</body>
+<br /><br><br /><br><br /><br><br />
+<p><Strong><center> sabiu yau</center></Strong></p>
+</html>
---
+++ index.php
@@ -0,0 +1,123 @@
+<?php
+session_start ();
+function loginForm() {
+    echo '
+	<div class="form-group">
+	<th class="nav" align="center"><img src="images/logo.jpg" width="200" height="150"></th>
+	<strong><marquee behavior="scrollTop">WELCOME TO COMPUTER SCIENCE DEPARTMENT JIGPOLY Online Meeting Application</marquee></span></font></div></strong>
+
+	</br></br><a href="../choose.php">Goto Home Page</a>
+		<div id="loginform">
+			<form action="index.php" method="post">
+			<h1>Online Meeting Application</h1><hr/>
+				<label for="name">Please Enter Your popular Name To chat with people</label>
+				<input type="text" name="name" id="name" class="form-control" placeholder="Enter Your popular Name" value="'.$_SESSION['fullname'].'"/>
+				<input type="submit" class="btn btn-default" name="enter" id="enter" value="Enter" />
+			</form>
+		</div>
+	</div>
+   ';
+}
+ 
+if (isset ( $_POST ['enter'] )) {
+    if ($_POST ['name'] != "") {
+        $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
+        $cb = fopen ( "log.html", 'a' );
+        fwrite ( $cb, "<div class='msgln'><i> " . $_SESSION ['name'] . " has joined the chat.</i><br></div>" );
+        fclose ( $cb );
+    } else {
+        echo '<span class="error">Please Enter a Name</span>';
+    }
+}
+ 
+if (isset ( $_GET ['logout'] )) {
+    $cb = fopen ( "log.html", 'a' );
+    fwrite ( $cb, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has left the chat.</i><br></div>" );
+    fclose ( $cb );
+    // session_destroy ();
+    header ( "Location: index.php" );
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Meeting Application </title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<!--
+		*******************************************************************************
+
+		BY ALIYU AHMAD OF FUD
+
+		*******************************************************************************
+	-->
+<body bgcolor="skyblue">
+<?php
+	if (! isset ( $_SESSION ['name'] )) {
+	loginForm ();
+	} else {
+?>
+<div id="wrapper">
+	<div id="menu">
+	<h1>Online Meeting Application</h1><hr/>
+	</br></br><a href="../choose.php">Goto Home Page</a>
+		<p class="welcome"><b>HI - <a><?php echo $_SESSION['name']; ?></a></b></p>
+		<p class="logout"><a id="exit" href="#" class="btn btn-default">Exit Live Chat</a></p>
+	<div style="clear: both"></div>
+	</div>
+	<div id="chatbox">
+	<?php
+		if (file_exists ( "log.html" ) && filesize ( "log.html" ) > 0) {
+		$handle = fopen ( "log.html", "r" );
+		$contents = fread ( $handle, filesize ( "log.html" ) );
+		fclose ( $handle );
+
+		echo $contents;
+		}
+	?>
+	</div>
+<form name="message" action="">
+	<input name="usermsg" class="form-control" type="text" id="usermsg" placeholder="Create A Message" />
+	<input name="submitmsg" class="btn btn-default" type="submit" id="submitmsg" value="Send" />
+</form>
+</div>
+<script type="text/javascript">
+$(document).ready(function(){
+});
+$(document).ready(function(){
+    $("#exit").click(function(){
+        var exit = confirm("Are You Sure You Want To Leave This Page?");
+        if(exit==true){window.location = 'index.php?logout=true';}     
+    });
+});
+$("#submitmsg").click(function(){
+        var clientmsg = $("#usermsg").val();
+        $.post("post.php", {text: clientmsg});             
+        $("#usermsg").attr("value", "");
+        loadLog;
+    return false;
+});
+function loadLog(){    
+    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+    $.ajax({
+        url: "log.html",
+        cache: false,
+        success: function(html){       
+            $("#chatbox").html(html);       
+            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
+            if(newscrollHeight > oldscrollHeight){
+                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal');
+            }              
+        },
+    });
+}
+setInterval (loadLog, 1500);
+</script>
+<?php
+}
+?>
+</body>
+<p><Strong><center></center></Strong></p>
+</html>
